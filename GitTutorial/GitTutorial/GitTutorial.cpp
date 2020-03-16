ea1f347e39c5456d772bff23746b83d8f91f8b86
tess89 <tess20000128@gmail.com>
2020-03-16 17:16:59 +0800

    Tutorial 3

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -8,7 +8,16 @@ bool Guess(int number) {
 		Random r;
 		target = r.Next() % 100 + 1;
 	}
-	if (number == target)return true;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else if (number > target) std::cout << "Smaller" << std::endl;
+	else if (number < target) std::cout << "Bigger" << std::endl;
+	return false;
+	}
+	else std::cout << "Wrong" << std::endl;
 	return false;
 }
 int main(array<System::String ^> ^args)
